aeb0aca65ca2e38c7133cf2223065d6f7b8cdb62
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-04-05 15:48:34 +0000

    Create FunctionalGreeting component and import to Appp.js

---
+++ my-app/src/components/FunctionalGreeting.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FunctionalGreeting = () => <h1>Hello from React!</h1>;
+// This is a stateless functional component, it has no interactive feature
+// and will not change but can be reused
+
+export default FunctionalGreeting;
