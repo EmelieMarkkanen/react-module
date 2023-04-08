3019161998c122c3c385b73f0f39dd453ffeaa13
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-04-08 13:22:37 +0000

    Add stateful class component with prevState and prevProps, increment button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
     <div className="App">
       {/* <FunctionalGreeting /> */}
       {/* <FunctionalGreetingWithProps greeting= "nice to meet you!" name="Michael" age="33" /> */}
-      { /*<StatefulGreeting greeting= "I'm a stateful class component!" name ="Theo" /> */}
-      <StatefulGreetingWithCallback greeting= "I'm a stateful class component!" name ="Theo" />
+      {/* <StatefulGreeting greeting= "I'm a stateful class component!" name ="Theo" /> */}
+      {/* <StatefulGreetingWithCallback greeting= "I'm a stateful class component!" name ="Theo" /> */}
+      <StatefulGreetingWithPrevState greeting= "I'm a stateful class component with an interactive button and greeting" name="Sam"/>
     </div>
   );
 }
