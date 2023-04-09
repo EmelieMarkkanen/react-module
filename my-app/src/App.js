4adf0892c6202b64b8fb2fa29c59afb182d86372
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-04-09 13:02:17 +0000

    Rendering lists

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,12 +12,14 @@ import ConditionalRenderingClass from './components/ConditionalRenderingClass';
 import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
 import NestingComponents from './components/NestingComponents';
 import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+import RenderingLists from './components/RenderingLists';
 
 function App() {
   return (
     <div className="App">
-      <MethodsAsPropsParent />
-      {/*<NestingComponents />
+      <RenderingLists />
+      {/*<MethodsAsPropsParent />
+      <NestingComponents />
       <ConditionalRenderingClass />
       <ConditionalRenderingFunctional connected={true}/>
       <EventBinding />
