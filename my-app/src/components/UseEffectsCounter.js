5b4093f02918e057a6d1d7300c28b387ba32b80e
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-04-12 19:23:37 +0000

    useEffect hook timer

--- my-app/src/components/UseEffectsCounter.js
+++ my-app/src/components/UseEffectsCounter.js
@@ -2,14 +2,25 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectsCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
+
     useEffect(() => {
+      console.log("count 1 effect")
         document.title = count
-    });
+    }, [count]);
+    useEffect(() => {
+      console.log("Creating timer..")
+      const interval = setInterval(() => {
+        setTime(time => time + 1)
+      }, 1000);
+    }, []);
+
   return (
     <div>
         <button onClick={() => setCount(count => count +1)}>
             Increment count ({count})
         </button>
+        <h2>Time is {time}</h2>
     </div>
   )
 }
